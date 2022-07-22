94b335a6315a3c11291ad906898635a53435d940
ayleafs <luaq@protonmail.com>
2022-07-22 19:23:11 -0400

    fix tabs

--- src/main/java/me/sixteen_/sort/Sort.java
+++ src/main/java/me/sixteen_/sort/Sort.java
@@ -16,51 +16,51 @@ import org.lwjgl.glfw.GLFW;
 import java.io.File;
 
 public class Sort implements ClientModInitializer {
-	@Getter private static Sort instance;
+    @Getter private static Sort instance;
 
-	@Getter private ConfigHandler<SortConfig> handler;
-	@Getter private SortConfig config;
+    @Getter private ConfigHandler<SortConfig> handler;
+    @Getter private SortConfig config;
 
-	private KeyBinding keyBinding;
+    private KeyBinding keyBinding;
 
-	@Override
-	public void onInitializeClient() {
-		instance = this;
+    @Override
+    public void onInitializeClient() {
+        instance = this;
 
-		handler = new ConfigHandler<>(new File("config", "sort_config.json"), SortConfig.class);
-		config  = handler.readConfig();
+        handler = new ConfigHandler<>(new File("config", "sort_config.json"), SortConfig.class);
+        config  = handler.readConfig();
 
-		// when the client shuts down we want to save the config
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> handler.populateConfig(config)));
+        // when the client shuts down we want to save the config
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> handler.populateConfig(config)));
 
-		// add the keybind to the keybinding menu
-		keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding("sort.keybind", GLFW.GLFW_KEY_R, "sort.name"));
-		this.registerKeybindEvents();
-	}
+        // add the keybind to the keybinding menu
+        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding("sort.keybind", GLFW.GLFW_KEY_R, "sort.name"));
+        this.registerKeybindEvents();
+    }
 
-	private void registerKeybindEvents() {
-		// whenever a new container GUI screen is opened, and it's a container, add a keybind listener
-		ScreenEvents.AFTER_INIT.register((client, screen, width, height) -> {
-			// it isn't a container so we don't care
-			if (!isContainer(screen)) {
-				return;
-			}
+    private void registerKeybindEvents() {
+        // whenever a new container GUI screen is opened, and it's a container, add a keybind listener
+        ScreenEvents.AFTER_INIT.register((client, screen, width, height) -> {
+            // it isn't a container so we don't care
+            if (!isContainer(screen)) {
+                return;
+            }
 
-			ScreenKeyboardEvents.afterKeyPress(screen).register((container, key, scanCode, modifiers) -> {
-				if (!keyBinding.matchesKey(key, scanCode)) {
-					return;
-				}
+            ScreenKeyboardEvents.afterKeyPress(screen).register((container, key, scanCode, modifiers) -> {
+                if (!keyBinding.matchesKey(key, scanCode)) {
+                    return;
+                }
 
-				ScreenHandler screenHandler = ((ScreenHandlerProvider<?>) container).getScreenHandler();
-				SortUtils.sort(screenHandler);
-			});
-		});
-	}
+                ScreenHandler screenHandler = ((ScreenHandlerProvider<?>) container).getScreenHandler();
+                SortUtils.sort(screenHandler);
+            });
+        });
+    }
 
-	private boolean isContainer(Screen screen) {
-		return screen instanceof GenericContainerScreen || //
-				screen instanceof ShulkerBoxScreen || //
-				screen instanceof Generic3x3ContainerScreen || //
-				screen instanceof HopperScreen;
-	}
+    private boolean isContainer(Screen screen) {
+        return screen instanceof GenericContainerScreen || //
+                screen instanceof ShulkerBoxScreen || //
+                screen instanceof Generic3x3ContainerScreen || //
+                screen instanceof HopperScreen;
+    }
 }
